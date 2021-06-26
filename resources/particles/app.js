1870cee78d8c0e923b8e2c195dbeaeb5801a02e2
thekristopl <kristopalka@gmail.com>
2021-06-26 19:09:26 +0200

    kind of immage gallery :/

--- resources/particles/app.js
+++ resources/particles/app.js
@@ -112,7 +112,7 @@ particlesJS('particles-js',
           "distance": 200
         },
         "push": {
-          "particles_nb": 4
+          "particles_nb": 3
         },
         "remove": {
           "particles_nb": 2
