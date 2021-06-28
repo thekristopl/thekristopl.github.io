bb6924e69c1a646768c9993690b6af79fa77bddd
thekristopl <kristopalka@gmail.com>
2021-06-28 21:47:57 +0200

    complete header :)

--- scripts/load-languages.js
+++ scripts/load-languages.js
@@ -8,6 +8,7 @@ function loadLanguage (lang) {
 function loadPolish () {
     $.getJSON("resources/pl.json", function (json) {
         loadLanguage(json);
+        $("html").attr('lang', 'pl');
     }).fail(function (){
         alert("Nie można załadować języka polskiego :/");
     });
@@ -18,18 +19,15 @@ function loadPolish () {
 function loadEnglish () {
     $.getJSON("resources/en.json", function (json) {
         loadLanguage(json);
+        $("html").attr('lang', 'en');
     }).fail(function (){
         alert("Can not load english language :/");
     });
 }
 
-$('#polish-btn').on('click', function () {
-    loadPolish();
-});
+$('#polish-btn').on('click', loadPolish);
 
-$('#english-btn').on('click', function (){
-    loadEnglish();
-});
+$('#english-btn').on('click', loadEnglish);
 
 
 
