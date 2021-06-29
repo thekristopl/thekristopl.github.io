f765d46e9862ba68c8692c0d6b5170ad1d6db45e
thekristopl <kristopalka@gmail.com>
2021-06-29 16:26:25 +0200

    sports achievements

--- scripts/scroll-header.js
+++ scripts/scroll-header.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
 
 function scrollFunction() {
     let max_opacity = 0.9;
-    let non_opacity_height = 300;
+    let non_opacity_height = 200;
     if ($(window).scrollTop() <= non_opacity_height) {
         $("#navigation-bar").css("backgroundColor",
             "rgba(17,17,17," + max_opacity * ($(window).scrollTop()/non_opacity_height) + ")");
